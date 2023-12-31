15d90acb9058541257a1bab88dae0acc3a35e897
ivenyao <ivenyao820109@gmail.com>
2023-12-31 02:14:53 -0800

    fix

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -3,7 +3,7 @@ import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
 import { Metadata } from 'next';
 export const metadata: Metadata = {
-    title: 'Create',
+    title: 'Create Invoice',
   };
 export default async function Page() {
   const customers = await fetchCustomers();
