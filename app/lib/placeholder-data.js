7e2ef760ac6bf1070da7ea1eba6cf85329eb4048
ivenyao <ivenyao820109@gmail.com>
2024-01-16 02:35:55 -0800

    -

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -1,14 +1,5 @@
 // This file contains placeholder data that you'll be replacing with real data in the Data Fetching chapter:
 // https://nextjs.org/learn/dashboard-app/fetching-data
-const users = [
-  {
-    id: '410544b2-4001-4271-9855-fec4b6a6442a',
-    name: 'User',
-    email: 'user@nextmail.com',
-    password: '123456',
-  },
-];
-
 const members = [
   {
     id: 'ed218cf0-339e-4d54-9d66-2a0ccb062553',
